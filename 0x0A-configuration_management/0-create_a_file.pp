58b4fdbb0a9d761000bacbbc7b1759404f0afd7b
Viestar <m.silverbuckson@gmail.com>
2023-08-25 23:13:06 +0300

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# creates a file called school in the /tmp directory
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
