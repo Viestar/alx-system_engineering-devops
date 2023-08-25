c229a96e7478c13db05f04113b6266cb94728c94
Viestar <m.silverbuckson@gmail.com>
2023-08-25 23:44:10 +0300

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,9 +1,6 @@
-# kill_process.pp
-
-# Define the exec resource to kill the process
+# kills a process
 exec { 'killmenow':
-  command     => '/usr/bin/pkill killmenow',  # Replace with the actual process name
-  path        => ['/bin', '/usr/bin'],        # Specify search paths for the command
-  user        => 'root',                      # User to run the command as
-  refreshonly => true,                        # Only run when explicitly refreshed
+  command  => '/usr/bin/pkill killmenow',
+  provider => 'shell',
+  returns  => [0, 1],
 }
