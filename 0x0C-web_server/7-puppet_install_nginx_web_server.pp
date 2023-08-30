79360fb3aee047ae3f3b2c91a7a1d16f7cfcf490
Viestar <m.silverbuckson@gmail.com>
2023-08-30 03:59:09 +0300

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,31 @@
-# Define the Nginx class
 class nginx {
+  package { 'nginx':
+    ensure => installed,
+  }
+
+  service { 'nginx':
+    ensure  => running,
+    enable  => true,
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => template('nginx/default.conf.erb'),
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+
+  file { '/var/www/html/index.html':
+    ensure  => file,
+    content => 'Hello World!',
+    require => Package['nginx'],
+  }
+}
+
+include nginx
+
+file { '/etc/nginx/default.conf.erb':class nginx {
   package { 'nginx':
     ensure => installed,
   }
@@ -29,6 +55,8 @@ include nginx
 
 # Define the Nginx vhost template
 file { '/etc/nginx/default.conf.erb':
+  ensure => file,
+  content => template('nginx/default.conf.erb'),
   ensure => file,
   content => template('nginx/default.conf.erb'),
 }
