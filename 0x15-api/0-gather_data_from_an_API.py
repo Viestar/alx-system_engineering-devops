775cfa64e3f28d1f14882c21db9485db766f5f5e
Viestar <m.silverbuckson@gmail.com>
2023-09-25 17:38:34 +0300

    Initial deposit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" Uses REST API to return information about an Employee """
+import sys
+
+import requests
+
+
+def employee_todo_progress(emp_id):
+
+    api_url = "https://jsonplaceholder.typicode.com"
+    todo_url = f"{api_url}/todos?userId={emp_id}"
+    user_url = f"{api_url}/users/{emp_id}"
+    try:
+        # Fetching employee credentials
+        user_response = requests.get(user_url)
+        emp_details = user_response.json()
+        emp_name = emp_details.get('name')
+
+        # Fetching todo list
+        todo_response = requests.get(todo_url)
+        todo_list = todo_response.json()
+        total_t = len(todo_list)
+        comp_t = 0
+        comp_tasks_list = []
+        for task in todo_list:
+            if task['completed']:
+                comp_tasks_list.append(task)
+                comp_t += 1
+
+        # Formatting and printing the data to standard output
+        print(f"Employee {emp_name} is done with tasks ({comp_t}/{total_t})")
+        for task in comp_tasks_list:
+            print(f"\t {task['title']}")
+
+    except requests.exceptions.RequestException as int_error:
+        print(f"Error: {int_error}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(1)
+
+    emp_id = (sys.argv[1])
+    employee_todo_progress(int(emp_id))
---
+++ 0x15-api/README.md
@@ -0,0 +1,8 @@
+# Table of contents
+
+![APIS](./assets/)
+
+Task  | Code | Description
+----- | ------ | -----------
+[0x00] | [REAMDE](./README.md) | Project Documentation.
+[0x00] | [Gather data from an API](./0-gather_data_from_an_API.py) | Python script that, uses REST API, with a given employee ID as an argument to return information about his/her TODO list progress formatted to standard output.
