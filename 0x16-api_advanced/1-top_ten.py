9bb847cd7a8566ed90eaff9b0a0b269798796584
Viestar <m.silverbuckson@gmail.com>
2023-10-10 20:26:03 +0300

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,12 +5,12 @@ import requests
 
 def top_ten(subreddit):
     """Queries the API for titles of the first 10 hot reddit posts """
-    subs = requests.get(f"https://www.reddit.com/r/{subreddit}\
+    subs = requests.get(f"https://www.reddit.com/r/{subreddit} \
                         /hot.json?limit=10",
                         headers={"User-Agent": "My-User-Agent"},
                         allow_redirects=False)
     if subs.status_code >= 300:
-        return 0
+        print('None')
     else:
         [print(child.get("data").get("title"))
          for child in subs.json().get("data").get("children")]
