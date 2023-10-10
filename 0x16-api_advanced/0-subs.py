5e0694943ca262081525f4d0089ad4350394c6a6
Viestar <m.silverbuckson@gmail.com>
2023-10-10 15:34:05 +0300

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" API querying module """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Queries the API for the total number of a subreddit subscribers """
+    subs = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
+                        headers={"User-Agent": "My-User-Agent"},
+                        allow_redirects=False)
+    if subs.status_code >= 300:
+        return 0
+
+    return subs.json().get("data").get("subs")
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,7 @@
+![api_ad](./assets/Screenshot%20from%202023-10-10%2015-23-56.png)
+
+## Table of contents
+
+Task | code | Description
+------ | ----- | -----------
+[0x00] | [0-subs.py](./0-subs.py) | Python function that Queries the API for the total number of a subreddit subscribers
