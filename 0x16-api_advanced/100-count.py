6d27e9ae4f4ea95d5650e3c7e9f6a5e31a9794dd
Viestar <m.silverbuckson@gmail.com>
2023-10-12 22:56:23 +0300

    Revert "100-count.py"
    This reverts commit c879f61c509c17d60c49553286923db8300decc5.

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,43 +5,43 @@ import requests
 
 def count_words(subreddit, word_list, word_count={}, after=None):
     """Queries Reddit API for word_list occurrences in hot posts titles """
-    if not word_list:
-        return
-    if subreddit is not isinstance(subreddit, str) or None:
-        return
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json",
-    head = {"User-Agent": "MyRedditBot/1.0"}
-    args = {"limit": 100, "after": after}
-    try:
-        response = requests.get(url, headers=head,
-                                params=args, allow_redirects=False)
+    user_info = requests.get(f"https://www.reddit.com/r/{subreddit}/hot.json",
+                             params={"after": after},
+                             headers={"User-Agent": "My-User-Agent"},
+                             allow_redirects=False)
+    if user_info.status_code == 200:
+        user_info_json = user_info.json()
 
-        if response.status_code == 200:
-            data = response.json()
+        hot_list = [child.get("data").get("title")
+                    for child in user_info_json
+                    .get("data")
+                    .get("children")]
+        if hot_list:
 
-            posts = data.get("data", {}).get("children", [])
-            titles = [post.get("data", {}).get("ttl", "").lower()
-                      for post in posts]
+            word_list = list(dict.fromkeys(word_list))
 
-            for ttl in titles:
-                for kywd in word_list:
-                    if (kywd.lower() in ttl
-                            and (len(kywd) + ttl.count(kywd.lower()) ==
-                                 ttl.count(kywd.lower() + ' ') +
-                                 ttl.count(' ' + kywd.lower()) +
-                                 ttl.count(' ' + kywd.lower() + ' '))):
-                        word_count[kywd.lower()] = word_count.get(
-                            kywd.lower(), 0) + 1
+            if word_count == {}:
+                word_count = {word: 0 for word in word_list}
 
-            after = data.get("data", {}).get("after")
-            if after:
-                return count_words(subreddit, word_list, after, word_count)
+            for title in hot_list:
+                split_words = title.split(' ')
+                for word in word_list:
+                    for s_word in split_words:
+                        if s_word.lower() == word.lower():
+                            word_count[word] += 1
+
+            if user_info_json.get("data").get("after"):
+                return count_words(subreddit, word_list, word_count,
+                                   user_info_json.get("data").get("after"))
+
             else:
-                print(word_count)
-
-        else:
-            return
-
-    except Exception:
-        return
+                sorted_counts = sorted(
+                    word_count.items(), key=lambda kv: kv[0])
+                sorted_counts = sorted(word_count.items(),
+                                       key=lambda kv: kv[1], reverse=True)
+                for key, value in sorted_counts:
+                    if value != 0:
+                        print(f"{key}: {value}")
+        return None
+    return None
