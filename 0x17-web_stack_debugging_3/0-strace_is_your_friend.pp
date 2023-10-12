ece682b4caea90fb585dfb6d873d1df0c31c5a0a
Viestar <m.silverbuckson@gmail.com>
2023-10-12 11:28:59 +0300

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,7 @@
 # Puppet Script that Fixes a wordpress site running on apache2
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo systemctl restart apache2.service',
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+$config_file = '/var/www/html/wp-settings.php'
+
+exec { 'edit_file':
+  command => "sed -i 's/phpp/php/g' ${config_file}",
+  path    => ['/bin','/usr/bin']
 }
